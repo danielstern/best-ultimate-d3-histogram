814e3d4d874a14bf1d4031ffda8fa428bf052ffb
Daniel Stern <daniel@danielstern.ca>
2015-02-01 20:51:53 -0500

    added ng-repeat bar chart! huzzah!

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,83 +1,118 @@
-console.log('\'Allo \'Allo!');
-
-var margin = {top: 100, right: 20, bottom: 30, left: 80},
-  width = 960 - margin.left - margin.right,
-  height = 500 - margin.top - margin.bottom;
-
-var x = d3.scale.ordinal()
-  .rangeRoundBands([0, width], .1);
-
-var y = d3.scale.linear()
-  .range([height, 0]);
-
-var xAxis = d3.svg.axis()
-  .scale(x)
-  .orient("bottom");
-
-var yAxis = d3.svg.axis()
-  .scale(y)
-  .orient("left")
-  .ticks(10);
-
-var svg = d3.select("body").append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-d3.json('data.json',function(e,data){
-  x.domain(data.map(function(d) { return d.id; }));
-  y.domain([0, d3.max(data, function(d) { return d.value; })]);
-
-  svg.append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + height + ")")
-    .call(xAxis)
-    .append("text")
-    .attr("y", 10)
-    .attr("dy", ".71em")
-    .attr("x",'90px')
-    .style("text-anchor", "end")
-    .text("Genome #");
-
-  svg.append("g")
-    .attr("class", "y axis")
-    .call(yAxis)
-    .append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 6)
-    .attr("dy", ".71em")
-    .style("text-anchor", "end")
-    .text("Value");
 
 
-  var color = d3.scale.category20();
+angular.module("test",[])
+.run(function($rootScope){
+    d3.json('data.json',function(e,root){
 
-  svg.selectAll(".bar")
-    .data(data)
-    .enter().append("rect")
-    .attr("class", "bar")
-    .attr("x", function(d) { return x(d.id); })
-    .attr("width", x.rangeBand())
-    .attr('rx','0%')
-    .attr('ry','0%')
-    .attr("y", function(d) { return height; })
-    .attr("height", function(d) { return 0; })
-    .attr('fill',function(d,i){
-      return color(i);
+      var width = 950,
+        height = 500;
+
+
+      var colorz = d3.scale.category20b();
+
+      var color = d3.scale.ordinal()
+        .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+      color.domain(d3.keys(root[0]).filter(function(key) { return key == 'id' }));
+
+      var svg = d3.select("body").append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .append("g")
+
+
+      var y0 = 0;
+      root.forEach(function(d){
+        d.y0 = y0;
+        var y1 = y0 += d.value;
+        d.y1 = y1;
+      });
+
+      var y = d3.scale.linear();
+      //y.domain([0,10]);
+      y.domain([0,root[root.length-1].y1]);
+      y.range([0,width]);
+
+      svg.selectAll('rect')
+        .data(root)
+        .enter().append('rect')
+        .attr('x',function(d){return y(d.y0)})
+        .attr('width',function(d){return y(d.y1 - d.y0)})
+        .attr('height',40)
+        .attr('fill',function(d,i){return colorz(i)})
+
+
+
+      $rootScope.data = root;
+
+      $rootScope.$apply();
+
+
     })
-    .transition()
-    .duration(300)
-    .delay(function(d,i){return i * 330})
-    .attr("height", function(d) { return height - y(d.value); })
-    .attr("y", function(d) { return y(d.value); })
 
-    .transition()
-    .duration(660)
-    .delay(5000)
-    .attr('rx','50%')
-    .attr('ry','50%')
-    .attr('height',50)
-    .attr('width',50)
 
-})
+  })
+.directive('chart',function(){
+    return {
+      restrict:'AE',
+      scope:{
+        thing:'='
+      },
+      controller:function($scope,$element){
+        var margin = {top: 2, right: 2, bottom: 3, left: 4},
+          width = 100 - margin.left - margin.right,
+          height = 30 - margin.top - margin.bottom;
+
+        var x = d3.scale.ordinal()
+          .rangeRoundBands([0, width], .1);
+
+        var y = d3.scale.linear()
+          .range([height, 0]);
+
+        var colors = d3.scale.category20c();
+
+
+
+        var svg = d3.select($element[0]).append("svg")
+          .attr("width", width + margin.left + margin.right)
+          .attr("height", height + margin.top + margin.bottom)
+          .append("g")
+          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        var data = [];
+        var i = 7;
+        while (i--) {
+          data.push({
+            id:i,
+            a:Math.random()*10,
+            b:Math.random()*10,
+            c:Math.random()*10,
+            d:Math.random()*10,
+            e:Math.random()*10
+          })
+        };
+          x.domain(data.map(function(d) { return d.id; }));
+          y.domain([0, d3.max(data, function(d) { return d.id; })]);
+
+
+          svg.selectAll(".bar")
+            .data(data)
+            .enter().append("rect")
+            .attr("class", "bar")
+            .attr("x", function(d) { return x(d.id); })
+            .attr("width", x.rangeBand())
+            .attr("y", function(d) { return y(d.a); })
+            .attr('fill',function(d){return colors(d.id)})
+            .attr("height", function(d) { return height - y(d.a); });
+
+
+        function type(d) {
+          d.value = +d.value;
+          return d;
+        }
+
+      }
+
+    }
+  })
+
